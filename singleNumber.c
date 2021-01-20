b4edd95b2a706bcbc51299c6b60b61ed7434c5fb
haoyu0970624763 <haoyu0970624763@gmail.com>
2021-01-20 16:59:07 +0800

    Recover this file to original state because another version have bug

--- singleNumber.c
+++ singleNumber.c
@@ -4,16 +4,17 @@
 int singleNumber(int *nums, int numsSize)
 {
     int apprear1 = 0, apprear2 = 0, apprear3 = 0;
-    int tmp1=0,tmp2=0;
+    int tmp1=0,tmp2=0,tmp3=0;
     for(int i=0;i<numsSize;i++) {
-
-        apprear1 = (~(apprear2 | apprear1) & nums[i]) | apprear1 & ~nums[i] | apprear3 & nums[i];
+        apprear1 = (~(tmp2 | tmp1) & nums[i]) | tmp1 & ~nums[i] | tmp3 & nums[i];
         apprear2 = tmp1 & nums[i] | tmp2 & ~nums[i];
         apprear3 = tmp2 & nums[i] | apprear3 & ~nums[i];
         tmp1 = apprear1;
         tmp2 = apprear2;
+        tmp3 =apprear3;
     }
     return apprear1;
+
 }
 
 void main()
